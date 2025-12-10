99d59e4ca10650e34729fc51a18182941e493b1d
en <enqiao2009@gmail.com>
2025-12-10 16:19:53 -0500

    created the base environment for a singular creature I'd say i'm pretty proud of this code. it follows good programming practices quite well. lots of abstractions and stuff. anyways, there's a 2D grid environment and a step function for actions. a creature class as well for keeping position and stuff like that

---
+++ environment.py
@@ -0,0 +1,114 @@
+import math
+import random
+
+
+class Creature:
+    def __init__(self):
+        self.pos = [0, 0]
+        self.age = 0
+        self.orientation = 0
+
+    def __repr__(self):
+        return f"Creature pos={self.pos}, age={self.age}, orientation={self.orientation}"
+
+    def forward_pos(self):
+        x, y = self.pos
+        if self.orientation == 0:
+            return [x-1, y]
+        elif self.orientation == 1:
+            return [x, y+1]
+        elif self.orientation == 2:
+            return [x+1, y]
+        else:
+            return [x, y-1]
+
+    def turn_left(self):
+        self.orientation = (self.orientation - 1) % 4
+
+    def turn_right(self):
+        self.orientation = (self.orientation + 1) % 4
+
+
+class Environment:
+    def __init__(self, creature, s=5, seed=1):
+        self.size = s
+        self.grid = [[0] * self.size for i in range(self.size)]
+        self.player = creature
+        self.player.pos = [s//2, s//2]
+        self.grid[s//2][s//2] = 1
+        random.seed(1)
+
+    def __repr__(self):
+        txt = ""
+        for row in self.grid:
+            for col in row:
+                txt += str(col) + " "
+            txt += "\n"
+        return txt[:-1]
+
+    def in_bounds(self, pos):
+        return 0 <= pos[0] < self.size and 0 <= pos[1] < self.size
+
+    def empty_positions(self):
+        positions = []
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.grid[i][j] == 0:
+                    positions.append((i, j))
+        return positions
+
+    def generate_food(self):
+        num_food = math.ceil(self.size ** 2 / 9)
+        chosen = random.sample(self.empty_positions(), num_food)
+        for i, j in chosen:
+            self.grid[i][j] = 2
+
+    def step(self, action):
+        ate = False
+        moved = False
+        if action == 0:
+            pass
+        elif action == 1:
+            new = self.player.forward_pos()
+            if self.in_bounds(new):
+                old = self.player.pos
+                x, y = new
+                if self.grid[x][y] == 2:
+                    ate = True
+
+                self.grid[old[0]][old[1]] = 0
+                self.grid[x][y] = 1
+                self.player.pos = new
+                moved = True
+        elif action == 2:
+            self.player.turn_left()
+        elif action == 3:
+            self.player.turn_right()
+        else:
+            raise ValueError("Invalid action", action)
+
+        self.player.age += 1
+
+        return {
+            "action": action,
+            "moved": moved,
+            "ate": ate,
+            "position": self.player.pos,
+            "orientation": self.player.orientation
+        }
+
+c = Creature()
+env = Environment(c)
+env.generate_food()
+print(repr(env))
+
+print(env.step(0))
+print(repr(env))
+print(env.player.age)
+print(env.step(3))
+print(repr(env))
+print(env.player.age)
+print(env.step(1))
+print(repr(env))
+print(env.player.age)
+
