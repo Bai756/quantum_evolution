01a4f7609903d95ea784d77f828d51dc3d1c9da7
en <enqiao2009@gmail.com>
2025-12-10 14:36:18 -0500

    Start of the project quick tests to familiarize myself with what rotations do to qubits and test to see what circuit will be the best for evolution.

---
+++ main.py
@@ -0,0 +1,39 @@
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
+import matplotlib.pyplot as plt
+from math import pi
+
+a = pi/2
+b = pi/2
+
+qc = QuantumCircuit(3)
+qc.ry(a, 0)
+qc.rx(b, 1)
+qc.cx(0, 1)
+qc.h(0)
+qc.h(2)
+qc.measure_all()
+
+for i in range(12):
+    qc = QuantumCircuit(3)
+    qc.ry(a, 0)
+    qc.rx(b, 1)
+    qc.cx(0, 1)
+    qc.h(0)
+    qc.h(2)
+    qc.measure_all()
+    sim = AerSimulator()
+    compiled = transpile(qc, sim)
+    job = sim.run(compiled, shots=1024)
+    result = job.result()
+    counts = result.get_counts()
+
+    sorted_items = sorted(counts.items())
+    sorted_dict = dict(sorted_items)
+    labels, values = sorted_dict.keys(), sorted_dict.values()
+
+    plt.bar(labels, values)
+    plt.show()
+
+    # a += pi / 6
+    # b += pi / 6
